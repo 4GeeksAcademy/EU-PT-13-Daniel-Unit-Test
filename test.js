e7e0d492360b7a56ae144567a30b18a02ae04c05
ZionSpardas <135459718+ZionSpardas@users.noreply.github.com>
2023-08-21 18:38:25 +0000

    Fixed

--- test.js
+++ test.js
@@ -38,12 +38,12 @@ test("One dollar should be 106.583 Yen", function(){
      expect(fromDollarToYen(3.5)).toBe(373.04166666666674); //1 dollar are 106.583 Yen, then 3.5 Dollars should be = ((3.5 / 1.2) * 127.9)
 })
 
-test("One yen should be 0.006 pounds", function(){
+test("One yen should be 0.006 pounds ", function(){
     //import the function from app.js
     const { fromYenToPound } = require('./app.js')
 
     // use the function like its suppoed to be used
-    const yens = fromYenToPound(3.5)
+    const pounds = fromYenToPound(3.5)
 
     // if 1 yen is 0.006 pounds, then 3.5 euros should be ((3.5 / 1.27.9 ) * 0.8)
     const expected =  (3.5 / 127.9) * 0.8; 
